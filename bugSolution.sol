01dfba6be5bd6bdfd20bf561adc8a9a87f38d100
BugHunter-X <askzenai@gmail.com>
2025-01-17 07:40:06 +0100

    Add bugSolution.sol file

---
+++ bugSolution.sol
@@ -0,0 +1,12 @@
+function transferOwnership(newOwner) {
+  require(newOwner != address(0), "Ownable: new owner is the zero address");
+  // Transfer the ownership.  We use emit here to emit the event OwnershipTransferred
+  emit OwnershipTransferred(_owner, newOwner);
+  _owner = newOwner;
+}
+
+function transferOwnership(newOwner) {
+    require(newOwner != address(0), "Ownable: new owner is the zero address");
+    _owner = newOwner;
+    emit OwnershipTransferred(_owner, newOwner);
+}
